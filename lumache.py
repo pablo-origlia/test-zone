d3e89686e2169382bbffda690fdfd59875ac1be3
pablo-origlia <pablo.origlia@estudiantes.unahur.edu.ar>
2024-06-12 11:02:14 -0300

    Some changes

--- lumache.py
+++ lumache.py
@@ -1,8 +1,8 @@
 """
-Lumache - Python library for cooks and food lovers.
+Zona de Pruebas - Python library for cooks and food lovers.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
 class InvalidKindError(Exception):
